6f1545624271d663bc8c26e366d87bda3fc541fc
Andrew Lanham <54631753+andrewdlanham@users.noreply.github.com>
2024-01-31 15:00:07 -0500

    Added basic movement

---
+++ ShooterGame/Intermediate/Build/Win64/UnrealGame/Inc/ShooterGame/UHT/ShooterGameGameModeBase.generated.h
@@ -0,0 +1,98 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "ShooterGameGameModeBase.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SHOOTERGAME_ShooterGameGameModeBase_generated_h
+#error "ShooterGameGameModeBase.generated.h already included, missing '#pragma once' in ShooterGameGameModeBase.h"
+#endif
+#define SHOOTERGAME_ShooterGameGameModeBase_generated_h
+
+#define FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_SPARSE_DATA
+#define FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_RPC_WRAPPERS
+#define FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_ACCESSORS
+#define FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAShooterGameGameModeBase(); \
+	friend struct Z_Construct_UClass_AShooterGameGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AShooterGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ShooterGame"), NO_API) \
+	DECLARE_SERIALIZER(AShooterGameGameModeBase)
+
+
+#define FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAShooterGameGameModeBase(); \
+	friend struct Z_Construct_UClass_AShooterGameGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AShooterGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ShooterGame"), NO_API) \
+	DECLARE_SERIALIZER(AShooterGameGameModeBase)
+
+
+#define FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AShooterGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AShooterGameGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AShooterGameGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AShooterGameGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AShooterGameGameModeBase(AShooterGameGameModeBase&&); \
+	NO_API AShooterGameGameModeBase(const AShooterGameGameModeBase&); \
+public: \
+	NO_API virtual ~AShooterGameGameModeBase();
+
+
+#define FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AShooterGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AShooterGameGameModeBase(AShooterGameGameModeBase&&); \
+	NO_API AShooterGameGameModeBase(const AShooterGameGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AShooterGameGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AShooterGameGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AShooterGameGameModeBase) \
+	NO_API virtual ~AShooterGameGameModeBase();
+
+
+#define FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_12_PROLOG
+#define FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_SPARSE_DATA \
+	FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_RPC_WRAPPERS \
+	FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_ACCESSORS \
+	FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_INCLASS \
+	FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_SPARSE_DATA \
+	FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_ACCESSORS \
+	FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SHOOTERGAME_API UClass* StaticClass<class AShooterGameGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_shooter_game_prototype_ShooterGame_Source_ShooterGame_ShooterGameGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
